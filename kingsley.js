2ede62aa8d7f258f6f6397f276ab0cb8caced5cb
Okoli Kingsley <106443177+Kingxleey@users.noreply.github.com>
2022-07-19 12:10:53 +0100

    Add files via upload

---
+++ kingsley.js
@@ -0,0 +1,13 @@
+let firstName = ("  kingsley");
+let myString = ("let me introduce myself, my name is" );
+let surName = (" Okoli");
+let intro = (myString + surName + firstName);
+console.log (intro);
+
+function trueOrFalse (isItTrue){
+    if (isItTrue){
+        return ("i work at seafix limited");
+    }
+    return ("i am currently an intern at seamfix limited");
+}
+console.log(trueOrFalse(false));
